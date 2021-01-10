abc4094306e2629e35d010a207a37fc08c426609
thiagotp <thiagoteixeira.ps@gmail.com>
2021-01-10 19:38:39 -0300

    lidando com módulos

---
+++ Hcode/modules/module-01.js
@@ -0,0 +1,15 @@
+console.log('Executando módulo-01.js')
+
+oculta = ()=>{
+    console.log('Executando função oculta')
+}
+
+executa = ()=>{
+    console.log('Executando a função executa')
+}
+
+welcome = `Aprendendo NodeJS`
+//REPL -> modo interativo do node
+
+
+module.exports = {executa, welcome}
---
+++ Hcode/modules/module-02.js
@@ -0,0 +1,11 @@
+const { executa } = require('./module-01')
+
+const modulo01 = require('./module-01')
+//require('./module-01')
+//const {executa} = require("./module-01")
+
+
+//executando um módulo importado
+modulo01.executa()
+//executa()
+modulo01.oculta()
